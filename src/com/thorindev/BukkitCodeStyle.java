a9c3655f0340dced96e4e034c6856dfd49039f7e
Chris Nethercott <c.nethercott@icloud.com>
2016-08-21 12:08:53 +0200

    Added onEnable and onDisable

--- src/com/thorindev/BukkitCodeStyle.java
+++ src/com/thorindev/BukkitCodeStyle.java
@@ -4,4 +4,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class BukkitCodeStyle extends JavaPlugin {
 	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
 }
