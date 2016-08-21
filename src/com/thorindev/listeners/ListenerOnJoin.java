477d62395b47c0e045664abb9d1599ccdb5b28e5
Chris Nethercott <c.nethercott@icloud.com>
2016-08-21 12:31:49 +0200

    Completed onJoin

--- src/com/thorindev/listeners/ListenerOnJoin.java
+++ src/com/thorindev/listeners/ListenerOnJoin.java
@@ -1,8 +1,12 @@
 package com.thorindev.listeners;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 import com.thorindev.BukkitCodeStyle;
+
+import net.md_5.bungee.api.ChatColor;
 
 public class ListenerOnJoin implements Listener {
 	
@@ -12,4 +16,8 @@ public class ListenerOnJoin implements Listener {
 		plugin = instance;
 	}
 
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		Player player = (Player) event.getPlayer();
+		player.sendMessage(ChatColor.GREEN + "Welcome to the server, " + player.getDisplayName());
+	}
 }
