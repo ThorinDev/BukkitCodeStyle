2bcce5b612620db840d5f7a7cad5f84e2eab74d8
Chris Nethercott <c.nethercott@icloud.com>
2016-08-21 12:26:46 +0200

    Added the ListenerOnJoin

---
+++ src/com/thorindev/listeners/ListenerOnJoin.java
@@ -0,0 +1,15 @@
+package com.thorindev.listeners;
+
+import org.bukkit.event.Listener;
+
+import com.thorindev.BukkitCodeStyle;
+
+public class ListenerOnJoin implements Listener {
+	
+	BukkitCodeStyle plugin;
+	 
+	public ListenerOnJoin(BukkitCodeStyle instance) {
+		plugin = instance;
+	}
+
+}
